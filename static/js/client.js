5a3222f48fa8dbec9225570488ca6e9d9b0fbdbc
rubences <91731294+rubences@users.noreply.github.com>
2023-07-07 15:20:01 +0000

    ya consume en tiempo real

--- static/js/client.js
+++ static/js/client.js
@@ -1,26 +1,39 @@
+// Crear la escena
 var scene = new THREE.Scene();
-var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+
+// Crear la cámara
+var camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 1000);
+camera.position.z = 5;
+
+// Crear el renderizador
 var renderer = new THREE.WebGLRenderer();
-
 renderer.setSize(window.innerWidth, window.innerHeight);
-document.body.appendChild(renderer.domElement);
+document.getElementById('renderCanvas').appendChild(renderer.domElement);
 
-$.getJSON('/data', function(data) {
-    for (var i = 0; i < data.length; i++) {
-        var geometry = new THREE.SphereGeometry(0.5, 32, 32);
-        var material = new THREE.MeshBasicMaterial({color: 0x00ff00});
-        var sphere = new THREE.Mesh(geometry, material);
+// Crear la geometría
+var geometry = new THREE.BoxGeometry(1, 1, 1);
 
-        sphere.position.set(data[i].x, data[i].y, data[i].z);
-        scene.add(sphere);
-    }
+// Crear el material
+var material = new THREE.MeshBasicMaterial({color: 0x00ff00});
 
-    camera.position.z = 10;
+// Crear el cubo
+var cube = new THREE.Mesh(geometry, material);
 
-    var animate = function () {
-        requestAnimationFrame(animate);
-        renderer.render(scene, camera);
-    };
+// Añadir el cubo a la escena
+scene.add(cube);
 
-    animate();
-});
+// Función de animación
+function animate() {
+    requestAnimationFrame(animate);
+
+    // Rotar el cubo
+    cube.rotation.x += 0.01;
+    cube.rotation.y += 0.01;
+
+    // Renderizar la escena
+    renderer.render(scene, camera);
+};
+
+// Llamar a la función de animación
+animate();
+
